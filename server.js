10c4418e4bd286a455e4bac30295c5722b89c1cc
Dipu Thakur <149783027+diputhakur17@users.noreply.github.com>
2026-05-06 18:18:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import jsonServer from 'json-server'
 
+const server = jsonServer.create()
+const router = jsonServer.router('src/jobs.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(8000, () => {
+    console.log('JSON Server is running on port 8000 🚀')
+})
